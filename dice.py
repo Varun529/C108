88a1adfd6b8a26c18d5f70e78dcead1d3fd4238c
Varun529 <67922714+Varun529@users.noreply.github.com>
2022-02-14 20:46:30 -0500

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,14 @@
+import random
+import numpy as np
+import plotly.figure_factory as ff
+dice_result=[]
+#count=[]
+for i in range(0,100):
+    dice1=random.randint(1,6)
+    dice2=random.randint(1,6)
+    dice_result.append(dice1+dice2)
+    #count.append(i)
+    
+fig=ff.create_distplot([dice_result],["Result"] ,show_hist=False)
+fig.show()
+
---
+++ weight.py
@@ -0,0 +1,7 @@
+import csv
+import plotly.figure_factory as ff
+import pandas as pd
+
+df=pd.read_csv("data.csv")
+fig=ff.create_distplot([df["Weight(Pounds)"].tolist()],["Height"],show_hist=False)
+fig.show()
